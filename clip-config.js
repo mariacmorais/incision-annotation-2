929d1c5d95cf5a98615b9fb584f7a6b87c9ac320
Maria Clara Morais <claramorais182@gmail.com>
2025-11-19 21:00:15 -0500

    Changed clip name

--- clip-config.js
+++ clip-config.js
@@ -7,42 +7,42 @@ window.ANNOTATION_CLIPS = [
   {
     id: "clip_01",
     label: "Clip 1",
-    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/Video_01_K_02.mp4",
+    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/clip_01.mp4",
     poster: "",
   },
   // Clip 02 //
   {
     id: "clip_02",
     label: "Clip 2",
-    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/Video_02_K_03.mp4",
+    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/clip_02.mp4",
     poster: "",
   },
     // Clip 03 //
   {
     id: "clip_03",
     label: "Clip 3",
-    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/Video_03_Trimmed_00053_53001_0000054.mp4",
+    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/clip_03.mp4",
     poster: "",
   },
       // Clip 04 //
   {
     id: "clip_04",
     label: "Clip 4",
-    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/Video_04_00099_99001_0000101.mp4",
+    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/clip_04.mp4",
     poster: "",
   },
         // Clip 05 //
   {
     id: "clip_05",
     label: "Clip 5",
-    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/Video_05_00130_130001_0000132.mp4",
+    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/clip_05.mp4",
     poster: "",
   },
           // Clip 06 //
   {
     id: "clip_06",
     label: "Clip 6",
-    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/Video_06_00172_172001_0000174.mp4",
+    src: "https://raw.githubusercontent.com/mariacmorais/incision-annotation-2/main/clip_06.mp4",
     poster: "",
   },
 ];
